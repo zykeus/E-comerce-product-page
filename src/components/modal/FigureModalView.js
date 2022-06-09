4f9e271500325647b34732013dee3965ca323fdc
Bruno Rocha <b-robert@hotmail.com>
2022-06-08 22:01:59 -0300

    Fixing the image height in the modal component

--- src/components/modal/FigureModalView.js
+++ src/components/modal/FigureModalView.js
@@ -5,7 +5,7 @@ const FigureModalView = ({ hasOpenModal, FigureThumbController, FigureProductsCo
 	return createPortal(
 		<div className={` hidden ${hasOpenModal ? 'md:flex' : 'md:invisible'} fixed w-full min-h-screen left-0 top-0 bg-black z-40 flex justify-center items-center`}>
 			<div className='
-					absolute md:w-1/2 md:h-3/4 md-lg:w-1/3 flex flex-col z-[100]'>
+					absolute md:w-1/2 md:h-fit md-lg:w-1/3 flex flex-col z-[100]'>
 				<div className='relative h-full w-full'>{FigureProductsController}</div>
 				<div>{FigureThumbController}</div>
 				<button onClick={handleCloseModal} onKeyPress={handleCloseModal} className='absolute -top-10 text-white font-bold right-1 text-xl'>X</button>
